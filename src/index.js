1698d2fac9ef980f0e62c48cdfe178a2aa0c71b4
Himjyoti <127434307+HimjyotiSarma@users.noreply.github.com>
2024-01-04 22:27:56 +0530

    utils files and App setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 // require("dotenv").config({
 //   path: "./env",
 // });
+import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+
+const app = express();
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is connecting at ${process.env.PORT}`);
+    });
 
+    app.on("error", (error) => {
+      console.log(`Error in Listing to PORT :  ${error}`);
+      throw new Error(error);
+    });
+  })
+  .catch((error) => {
+    console.log(`MONGODB failed to Connect. ERROR: ->> ${error}`);
+  });
 /* 
 import { DB_NAME } from "./constants";
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// First Approach using Try Catch
+
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//   try {
+//     requestHandler(req, res, next);
+//   } catch (error) {
+//     res.status(err.status || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+// Second Approach Using Promises
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
